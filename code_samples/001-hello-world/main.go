06c78583d3f7bfb57f0c301a82939d3312a1e780
Mars Williams <marswilliams@Marss-Air.lan>
2020-11-12 16:31:06 -0500

    play

--- code_samples/001-hello-world/main.go
+++ code_samples/001-hello-world/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
+
+	foo()
+
 	fmt.Println("Hello World")
+
+	for i := 0; i < 100; i++ {
+		if i % 2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+		fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("And then we exited")
 }
